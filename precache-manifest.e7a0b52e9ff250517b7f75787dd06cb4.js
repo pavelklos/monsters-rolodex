c8fda269b5911be87b0bcf3a589ac9c3cca4d42d
pavelklos <pavelkloscz@gmail.com>
2019-07-24 13:13:10 +0200

    Updates

---
+++ precache-manifest.e7a0b52e9ff250517b7f75787dd06cb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40caef926d0b132d3b894bd74e1c5fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f7361278447ef24a9ff",
+    "url": "/monsters-rolodex/static/css/main.da31b116.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7f7361278447ef24a9ff",
+    "url": "/monsters-rolodex/static/js/main.4beb3c5f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
